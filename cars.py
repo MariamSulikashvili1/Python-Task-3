af08376ab77bd205fc4b8add58c49a7a76713dd0
MariamSulikashvili1 <116154200+MariamSulikashvili1@users.noreply.github.com>
2023-03-04 18:31:28 +0400

    Add files via upload
    Task 3

---
+++ cars.py
@@ -0,0 +1,16 @@
+# A class with one class attribute
+class Car:
+    # initializer with instance attributes
+    def __init__(self, color, brand, model, production_year,horse_power, is_sport_car = False ):
+        self.color = color
+        self.brand = brand
+        self.model = model
+        self.production_yaer
+        self.horse_power
+        self.is_sport_car
+
+
+def get_color(self):
+    return self._True
+
+
---
+++ main.py
@@ -0,0 +1,5 @@
+ from models class import cars
+ cars = cars("color", "brand", "model", "production_year","horse_power", "is_sport_car")
+ print(cars.color)
+
+
